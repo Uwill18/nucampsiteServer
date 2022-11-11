0fd48bfa9cc4141d7998606f70f180f850c7a7e8
Hoshi Holiday <hoshiholiday@gmail.com>
2022-11-11 14:39:05 -0600

    testing
    testing routers

--- models/campsite.js
+++ models/campsite.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// require('mongoose-currency').loadType(mongoose);
+// const Currency = mongoose.Types.Currency;
 
 const campsiteSchema = new Schema({
     name: {
@@ -20,7 +22,7 @@ const Campsite = mongoose.model('Campsite', campsiteSchema);
 module.exports = Campsite;
 
 
-
+//one
 // const mongoose = require('mongoose');
 // const Schema = mongoose.Schema;
 
@@ -58,31 +60,7 @@ module.exports = Campsite;
 //     timestamps: true
 // });
 
-
-
-
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// const commentSchema = new Schema({
-//     rating: {
-//         type: Number,
-//         min: 1,
-//         max: 5,
-//         required: true
-//     },
-//     text: {
-//         type: String,
-//         required: true
-//     },
-//     author: {
-//         type: String,
-//         required: true
-//     }
-// }, {
-//     timestamps: true
-// });
-
+//two
 // const campsiteSchema = new Schema({
 //     name: {
 //         type: String,
@@ -92,6 +70,23 @@ module.exports = Campsite;
 //     description: {
 //         type: String,
 //         required: true
+//     },
+//     image: {
+//         type: String,
+//         required: true
+//     },
+//     elevation: {
+//         type: Number,
+//         required: true
+//     },
+//     cost: {
+//         type: Currency,
+//         required: true,
+//         min: 0
+//     },
+//     featured: {
+//         type: Boolean,
+//         default: false
 //     },
 //     comments: [commentSchema]
 // }, {
